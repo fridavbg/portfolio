cd7ad74f0b9e31de558912389456024318233408
fridavbg <fridavbg@gmail.com>
2021-03-10 15:44:39 +0100

    photo changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
-import bkg from "../img/bkg2.jpg";
+import bkg from "../img/bkg4.jpg";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
@@ -34,15 +34,15 @@ function About() {
 				alt="waves crashing against a sandy beach"
 				className="absolute w-full"
 			/>
-			<div className="p-10 lg:pt-16 container mx-auto relative">
-				<section className="bg-green-600 rounded-lg shadow-2xl lg:flex p-20 md:flex p-10">
+			<div className="p-10 lg:pt-10 container mx-auto relative">
+				<section className="bg-gray-400 rounded-lg shadow-2xl lg:flex p-10 md:flex p-5">
 					<img
 						src={urlFor(author.authorImage).url()}
 						className="rounded lg:w-48 h-60 mr-8 md:w-16 h-16"
 						alt={author.name}
 					/>
 					<div className="text-lg flex flex-col justify-center">
-						<h1 className="cursive text-6xl text-green-800 mb-4 md: mt-2">
+						<h1 className="cursive text-6xl text-green-900 mb-4 md: mt-2">
 							Hi, I'm <span className="text-green-100">{author.name}</span>
 						</h1>
 						<div className="prose lg:prose-xl text-white">
