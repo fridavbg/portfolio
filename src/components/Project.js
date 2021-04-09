3114bf7ffb5f76fbb0b4082c324c455cfe043ea9
fridavbg <fridavbg@gmail.com>
2021-04-09 21:20:15 +0200

    Github repo in projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,6 +14,7 @@ function Project() {
 		description,
 		projectType,
 		link,
+		github,
 		tags
 		}`
 			)
@@ -24,51 +25,59 @@ function Project() {
 	if (!projectData) return <div>Loading...</div>;
 
 	return (
-		<main className="bg-green-100 min-h-screen p-12">
-			<section className="container mx-auto">
-				<h1 className="text-5xl flex justify-center mb-2 cursive">
-					My Projects
-				</h1>
-				<h2 className="text-lg text-gray-600 flex justify-center mb-12">
-					This is what I have been building lately.
-				</h2>
-				<section className="flex flex-wrap mt-2 mx-2">
-					{projectData &&
-						projectData.map((project, index) => (
-							<article className=" rounded-lg shadow-xl bg-white p-16 m-6 w-full">
-								<h3 className="text-gray-800 text-3xl font-bold m-2 hover:text-gray-700">
-									<a
-										href={project.link}
-										alt={project.title}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										{project.title}
-									</a>
-								</h3>
-								<div className="text-gray-500 text-xs space-x-4 m-4">
-									<span>
-										<strong className="font-bold">Uploaded on:</strong>:{" "}
-										{new Date(project.date).toLocaleString()}
-									</span>
-									<p className="m-6 text-lg text-gray-700 leading-relaxed">
-										{project.description}
-									</p>
-									<a
-										href={project.link}
-										rel="noopener noreferrer"
-										target="_blank"
-										className="text-gray-500 font-bold hover:underline hover:text-gray-400 text-xl"
-									>
-										View the project
-									</a>
-								</div>
-							</article>
-						))}
-				</section>
-			</section>
-		</main>
-	);
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center mb-2 cursive">
+          My Projects
+        </h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          This is what I have been building lately.
+        </h2>
+        <section className="flex flex-wrap mt-2 mx-2">
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className=" rounded-lg shadow-xl bg-white p-16 m-6 w-full">
+                <h3 className="text-gray-800 text-3xl font-bold m-2 hover:text-gray-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4 m-4">
+                  <span>
+                    <strong className="font-bold">Uploaded on:</strong>:{" "}
+                    {new Date(project.date).toLocaleString()}
+                  </span>
+                  <p className="m-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-gray-500 font-bold hover:underline hover:text-gray-400 text-xl"
+                  >
+                    View the project
+                  </a>
+                  <a
+                    href={project.github}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-gray-500 font-bold hover:underline hover:text-gray-400 text-xl"
+                  >
+                    Github repo
+                  </a>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
+  );
 }
 
 export default Project;
--- studio/schemas/project.js
+++ studio/schemas/project.js
@@ -1,51 +1,55 @@
 export default {
-	name: "project",
-	title: "Project",
-	type: "document",
-	fields: [
-		{
-			name: "title",
-			type: "string",
-		},
-		{
-			name: "date",
-			type: "datetime",
-		},
-		{
-			name: "place",
-			type: "string",
-		},
-		{
-			name: "description",
-			type: "text",
-		},
-		{
-			name: "projectType",
-			title: "Project type",
-			type: "string",
-			options: {
-				list: [
-					{ value: "plain/vanilla", title: "Plain/Vanilla" },
-					{ value: "frameworks", title: "Frameworks" },
-					{ value: "network", title: "Network" },
-				],
-			},
-		},
-		{
-			name: "link",
-			type: "url",
-		},
-		{
-			name: "tags",
-			type: "array",
-			of: [
-				{
-					type: "string",
-				},
-			],
-			options: {
-				layout: "tags",
-			},
-		},
-	],
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "date",
+      type: "datetime",
+    },
+    {
+      name: "place",
+      type: "string",
+    },
+    {
+      name: "description",
+      type: "text",
+    },
+    {
+      name: "projectType",
+      title: "Project type",
+      type: "string",
+      options: {
+        list: [
+          { value: "plain/vanilla", title: "Plain/Vanilla" },
+          { value: "frameworks", title: "Frameworks" },
+          { value: "network", title: "Network" },
+        ],
+      },
+    },
+    {
+      name: "link",
+      type: "url",
+    },
+    {
+      name: "github",
+      type: "url",
+    },
+    {
+      name: "tags",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+      options: {
+        layout: "tags",
+      },
+    },
+  ],
 };
